2c39711eea81c71f8cad710e1ebc96ab173ada0f
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-13 22:14:45 -0500

    Add Course view

---
+++ course/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Course(models.Model):
+    
+    product_code = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    image = models.URLField(max_length=500)
+    description = models.TextField(max_length=500)
+    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, name="user")
